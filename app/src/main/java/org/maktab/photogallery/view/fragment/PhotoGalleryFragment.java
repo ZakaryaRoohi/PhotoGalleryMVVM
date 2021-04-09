438165b2ae5c2a0a66f0013351f340b0e40bda7c
ZakaryaRoohi <zakarya.roohi@gmail.com>
2021-04-09 15:14:56 +0430

    Add DataBinding and move PhotoAdapter to new package (adapters)

--- app/src/main/java/org/maktab/photogallery/view/fragment/PhotoGalleryFragment.java
+++ app/src/main/java/org/maktab/photogallery/view/fragment/PhotoGalleryFragment.java
@@ -1,6 +1,5 @@
-package org.maktab.photogallery.controller.fragment;
+package org.maktab.photogallery.view.fragment;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,6 +15,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.squareup.picasso.Picasso;
 
 import org.maktab.photogallery.R;
+import org.maktab.photogallery.adapters.PhotoAdapter;
+import org.maktab.photogallery.databinding.FragmentPhotoGalleryBinding;
 import org.maktab.photogallery.model.GalleryItem;
 import org.maktab.photogallery.repository.PhotoRepository;
 
@@ -29,7 +31,9 @@ public class PhotoGalleryFragment extends Fragment {
 
     public static final int SPAN_COUNT = 3;
     public static final String TAG = "PGF";
-    private RecyclerView mRecyclerView;
+
+    private FragmentPhotoGalleryBinding mFragmentPhotoGalleryBinding;
+
     private PhotoAdapter mAdapter;
 
     private PhotoRepository mRepository;
@@ -67,82 +71,36 @@ public class PhotoGalleryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+//        View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+//        findViews(view);
 
-        findViews(view);
+        mFragmentPhotoGalleryBinding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_photo_gallery,
+                container,
+                false);
         initViews();
 
-        return view;
+        return mFragmentPhotoGalleryBinding.getRoot();
     }
 
-    private void findViews(View view) {
-        mRecyclerView = view.findViewById(R.id.photo_gallery_recyclerview);
-    }
+//    private void findViews(View view) {
+//        mRecyclerView = view.findViewById(R.id.photo_gallery_recyclerview);
+//    }
 
     private void initViews() {
-        mRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), SPAN_COUNT));
+        mFragmentPhotoGalleryBinding.photoGalleryRecyclerview.setLayoutManager(
+                new GridLayoutManager(getContext(), SPAN_COUNT));
     }
 
     private void setupAdapter(List<GalleryItem> items) {
-        mAdapter = new PhotoAdapter(items);
-        mRecyclerView.setAdapter(mAdapter);
+        mAdapter = new PhotoAdapter(getContext(),items);
+       mFragmentPhotoGalleryBinding.photoGalleryRecyclerview.setAdapter(mAdapter);
     }
 
-    private class PhotoHolder extends RecyclerView.ViewHolder {
 
-        private ImageView mImageView;
-        private GalleryItem mItem;
 
-        public PhotoHolder(@NonNull View itemView) {
-            super(itemView);
 
-            mImageView = itemView.findViewById(R.id.imageview_photo);
-        }
-
-        public void bindPhoto(GalleryItem item) {
-            mItem = item;
-
-            Picasso.get()
-                    .load(mItem.getUrl())
-                    .placeholder(R.mipmap.ic_android)
-                    .into(mImageView);
-        }
-    }
-
-    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
-
-        private List<GalleryItem> mItems;
-
-        public List<GalleryItem> getItems() {
-            return mItems;
-        }
-
-        public void setItems(List<GalleryItem> items) {
-            mItems = items;
-        }
-
-        public PhotoAdapter(List<GalleryItem> items) {
-            mItems = items;
-        }
-
-        @NonNull
-        @Override
-        public PhotoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(getContext())
-                    .inflate(R.layout.list_item_photo, parent, false);
-            return new PhotoHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull PhotoHolder holder, int position) {
-            holder.bindPhoto(mItems.get(position));
-        }
-
-        @Override
-        public int getItemCount() {
-            return mItems.size();
-        }
-    }
 
     /*private class FetchItemTasks extends AsyncTask<String, Void, List<GalleryItem>> {
 
---
+++ app/src/main/java/org/maktab/photogallery/viewModel/PhotoGalleryViewModel.java
@@ -0,0 +1,4 @@
+package org.maktab.photogallery.viewModel;
+
+public class PhotoGalleryViewModel {
+}
